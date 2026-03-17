c480561b8d68a89fb1c42cd4c127a5efac3745f5
Nakscott <gninphanna@gmail.com>
2026-03-17 13:20:34 +0700

    finsish the foundation fix some bug

--- src/command.py
+++ src/command.py
@@ -2,4 +2,4 @@ def start():
     return "Hi welcome to spodify trackiing bot, how can i help you"
 
 def user_spodify():
-    
+    return "This is your username and email_address"
--- src/main.py
+++ src/main.py
@@ -1,11 +1,62 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from command import start
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TOKEN = os.getenv("Telegram_API")
+BOT_USERNAME = "@SpodifyTrack_bot"
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("hello welcome to spodifyTracking app here you can know about your favorite artist and playlist")
+    await update.message.reply_text(start)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("here are all the list of command")
+
+#Response 
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+    if "hello" in processed:
+        return "hi welcome to my bot"
+    
+    if "how are you" in processed:
+        return "Im fine thank you"
+    
+    return "i do not understand"
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f"User ({update.message.chat.id}) in {message_type}: {text}")
+
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text = text.replace(BOT_USERNAME, "").strip()
+            response = handle_response(new_text)
+        else:
+            return
+    else:
+        response = handle_response(text)
+
+    await update.message.reply_text(response)
+
+
+if __name__ == "__main__":
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    print("Bot is running...")
+    app.run_polling()
+            
+
+
 
 
